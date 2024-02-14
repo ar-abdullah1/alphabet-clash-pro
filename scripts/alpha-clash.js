05913a1f2e88778b9625ec18b2ef39e05bd3ee4e
ar-abdullah <abdullahmr804@gmail.com>
2024-02-14 19:17:07 +0600

    added hide and show element js

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,17 @@
-function play (){
-    console.log("Play Star Now");
+// function play() {
+//   // step-1: hide the home screen. to hide the screen add the class hidden the home section
+//   const homeScreen = document.getElementById("home-screen");
+//   homeScreen.classList.add("hidden");
+//   //  console.log(homeScreen.classList);
+
+//   // show the playground
+//   const playgroundSection = document.getElementById("play-ground");
+//   playgroundSection.classList.remove("hidden");
+//   // console.log(playgroundSection.classList);
+// }
+
+
+function play( ){
+  hideElementById("home-screen");
+  showElementById("play-ground")
 }
